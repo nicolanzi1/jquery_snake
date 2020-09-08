07d2feaf919e05c18a2b041cdf0d1995f5db37b4
nicolanzi1 <nicolanzi@gmail.com>
2020-09-08 18:10:45 +0100

    Wrote classes and methods for snake and board

---
+++ js/board.js
@@ -0,0 +1,43 @@
+const Snake = require('./snake');
+
+class Board {
+    constructor(dim) {
+        this.dim = dim;
+
+        this.snake = new Snake(this);
+    }
+
+    static blankGrid(dim) {
+        const grid = [];
+
+        for (let i = 0; i < dim; i++) {
+            const row = [];
+            for (let j = 0; j < dim; j++) {
+                row.push(Board.BLANK_SYMBOL);
+            }
+            grid.push(row);
+        }
+
+        return grid;
+    }
+
+    render() {
+        const grid = Board.blankGrid(this.dim);
+
+        this.snake.segments.forEach( segment => {
+            grid[segment.i][segment.j] = Snake.SYMBOL;
+        });
+
+        const rowStrs = [];
+        grid.map( row => row.join("") ).join("\n");
+    }
+
+    validPosition(coord) {
+        return (coord.i >= 0) && (coord.i < this.dim) &&
+        (coord.j >= 0) && (coord.j < this.dim);
+    }
+}
+
+Board.BLANK_SYMBOL = ".";
+
+module.exports = Board;
